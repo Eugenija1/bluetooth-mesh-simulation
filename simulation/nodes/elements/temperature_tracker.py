f98c981dea82bb04c82c6e6871a8056d9b634cc2
Maryna05 <luka4ik.marina@gmail.com>
2020-04-05 12:57:44 +0200

    up

--- simulation/nodes/elements/temperature_tracker.py
+++ simulation/nodes/elements/temperature_tracker.py
@@ -1,4 +1,27 @@
 from simulation.nodes.elements import Element
 
 class TemperatureTracker(Element):
-    pass
+    def __init__(self, env):
+        self.__threshold = None
+        self.__env = env
+
+    @property
+    def threshold(self):
+        return self.__threshold
+
+    @threshold.setter
+    def threshold(self, value):
+        self.__threshold = value
+
+    def notify(self, newvalue):
+        if newvalue > self.__threshold:
+            print("newvalue > threshhold")
+
+
+
+
+
+
+
+
+
